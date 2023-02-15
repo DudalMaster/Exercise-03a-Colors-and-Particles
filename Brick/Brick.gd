b5c403f7e7d1db6abacb20675b98cb0043865170
DudalMaster <dmtibbs@iu.edu>
2023-02-14 22:33:19 -0500

    Complete

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -6,9 +6,25 @@ var dying = false
 
 func _ready():
 	position = new_position
+	if score >= 100:
+		$ColorRect.color = Color8(240,62,62)
+	elif score >= 90:
+		$ColorRect.color = Color8(174,62,201)
+	elif score >= 80:
+		$ColorRect.color = Color8(250,176,5)
+	elif score >= 70:
+		$ColorRect.color = Color8(55,178,77)
+	elif score >= 60:
+		$ColorRect.color = Color8(21,170,191)
+	elif score >= 50:
+		$ColorRect.color = Color8(247,131,172)
+	elif score >= 40:
+		$ColorRect.color = Color8(218,119,242)
+	else:
+		$ColorRect.color = Color8(134,124,150)
 
 func _physics_process(_delta):
-	if dying:
+	if dying and not $Confetti.emitting:
 		queue_free()
 
 func hit():
@@ -20,3 +36,4 @@ func die():
 	$ColorRect.hide()
 	Global.update_score(score)
 	get_parent().check_level()
+	$Confetti.emitting = true
